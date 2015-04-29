5a7c65d9a5f7e25f4e6a488200808656aabbcff2
Oleksandr Hnyp <Oleksandr_Hnyp@epam.com>
2015-04-29 20:36:02 +0300

    classpath change, added crossdomain request template

--- src/main/webapp/ui/js/myanalitics.js
+++ src/main/webapp/ui/js/myanalitics.js
@@ -26,3 +26,32 @@ function sendCurrentPageInfo() {
 function determineSessionId() {
 	
 }
+
+function createQueryString(data) {
+	var query = '?';
+	for (var key in data) {
+		query += key + "=" + data[key] + "&";
+	}
+	return query;
+}
+
+function makeCrossRequest(url, data) {
+	isCurrentlyRunning = true;
+	
+	var src = url + createQueryString(data);
+	var scriptElement = createScriptElement(CROSS_SCRIPT_ID, src);
+	$("head").first().append(scriptElement);
+	var response = $(scriptElement).text();
+	//$(scriptElement).remove();
+
+	isCurrentlyRunning = false;
+	
+	return response;
+}
+
+function createScriptElement(id, src) {
+	var scriptElement = $("<script>");
+	$(scriptElement).attr("id", id);
+	$(scriptElement).attr("src", src);
+	return scriptElement;
+}
